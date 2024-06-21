72ea3b177448169c5abcb4fd3ce8980eeebff834
raven.huang <raven.huang>
2024-06-21 10:13:52 +0800

    初始化项目

---
+++ redis-use-case/src/main/java/com/raven/uc/ruc/RedisUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.raven.uc.ruc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisUseCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisUseCaseApplication.class, args);
+    }
+
+}
---
+++ redis-use-case/src/test/java/com/raven/uc/ruc/RedisUseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raven.uc.ruc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisUseCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
